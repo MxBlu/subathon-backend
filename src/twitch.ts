c309b08dd415eafb1e189322f80c4c802dec7749
Jumail Mundekkat <jmundekkat@barnet.com.au>
2022-01-26 12:10:57 +0530

    Add more req/res interfaces for Twitch; change get() to generic call()

--- src/twitch.ts
+++ src/twitch.ts
@@ -7,6 +7,7 @@ import { Logger } from "./util/logger.js";
   Twitch API client
 */
 
+// https://dev.twitch.tv/docs/api/reference#get-users
 export interface GETUsersResponse {
   data: [
     { 
@@ -23,6 +24,64 @@ export interface GETUsersResponse {
       created_at: string;
     }
   ]
+}
+
+// https://dev.twitch.tv/docs/api/reference#create-eventsub-subscription
+export interface POSTEventSubRequest {
+  type: string;
+  version: string;
+  condition: APIEventSubCondition;
+  transport: APIEventSubTransport;
+}
+
+// https://dev.twitch.tv/docs/api/reference#create-eventsub-subscription
+export interface POSTEventSubResponse {
+  data: [
+    {
+      id: string;
+      status: string;
+      type: string;
+      version: string;
+      condition: APIEventSubCondition;
+      created_at: string;
+      transport: APIEventSubTransport;
+      cost: number;
+    }
+  ],
+  total: number;
+  total_cost: number;
+  max_total_cost: number;
+}
+
+export interface GETUsersResponse {
+  data: [
+    { 
+      id: string;
+      login: string;
+      display_name: string;
+      type: string;
+      broadcaster_type: string;
+      description: string;
+      profile_image_url: string;
+      offline_image_url: string;
+      view_count: number;
+      email: string;
+      created_at: string;
+    }
+  ]
+}
+
+// https://dev.twitch.tv/docs/eventsub/eventsub-reference#conditions
+// Just the ones we care - follow, subscribe, gift, bits
+interface APIEventSubCondition {
+  broadcaster_user_id?: string;
+}
+
+// https://dev.twitch.tv/docs/eventsub/eventsub-reference#transport
+interface APIEventSubTransport {
+  method: "webhook";
+  callback: string;
+  secret: string;
 }
 
 class AuthorizationError extends Error {
@@ -42,11 +101,12 @@ export class TwitchAPIClient {
 
   // Call GET /users on the Twitch API
   public async identifyUser(): Promise<GETUsersResponse> {
-    return await this.get('/users') as GETUsersResponse;
+    return await this.call('/users', 'GET') as GETUsersResponse;
   }
 
   // Make a GET request to the Twitch API
-  private async get(endpoint: string): Promise<unknown> {
+  private async call(endpoint: string, method: string, 
+      body: string | URLSearchParams = null): Promise<unknown> {
     // Ensure token is still valid
     if (! await this.validate()) {
       // If not, try a refresh first
@@ -58,11 +118,12 @@ export class TwitchAPIClient {
     }
     // Do the request
     const response = await fetch(`https://api.twitch.tv/helix${endpoint}`, {
-      method: "GET",
+      method: method,
       headers: {
         "Authorization": `Bearer ${this.token.token}`,
         "Client-Id": this.clientId
-      }
+      },
+      body: body
     });
     // If the request returned a bad response, return null;
     if (!response.ok) {
