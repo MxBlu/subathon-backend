31e84f63fc46af4f34abb1f068cdbd68a2d7f24b
Jumail Mundekkat <jmundekkat@barnet.com.au>
2022-01-26 16:23:14 +0530

    Add webhook creation and deletion on connection

--- src/clients.ts
+++ src/clients.ts
@@ -2,7 +2,8 @@ import * as crypto from "crypto";
 import * as WebSocket from 'ws';
 
 import { StoredToken } from "./auth.js";
-import { CLIENT_MAP_GC_INTERVAL, CLIENT_TIMEOUT } from "./constants.js";
+import { CLIENT_MAP_GC_INTERVAL, CLIENT_TIMEOUT, WEBHOOK_URI } from "./constants.js";
+import { TwitchAPIClient } from "./twitch.js";
 import { Logger } from "./util/logger.js";
 
 export const HEADER_SESSION_ID = "x-session-id";
@@ -63,19 +64,50 @@ class ClientMapImpl {
 
   // Setup a newly connected client
   // Assumes the session is present
-  public setupClient(sessionId: string, socket: WebSocket): void {
+  public async setupClient(sessionId: string, socket: WebSocket): Promise<void> {
     const clientInfo = this.getClient(sessionId);
     clientInfo.clientSocket = socket;
+
+    // Create a client with the user's credentials
+    const userClient = new TwitchAPIClient(clientInfo.clientToken);
+    // Get info about the current user
+    const userResponse = await userClient.identifyUser();
+    const userId = userResponse.data[0].id;
+    this.logger.debug(`Identified user for session: ${sessionId} as ${userId}`);
+    // Create a client with the app's credentials
+    const appClient = new TwitchAPIClient();
+    // Setup webhooks to listen to
+    const followWebhookResponse = await appClient.createEventSubSubscription(
+        "channel.follow", userId, WEBHOOK_URI, clientInfo.webhookSecret);
+    this.logger.debug(
+        `Created webhook for session: ${sessionId}, ${followWebhookResponse.data[0].id}`);
+    this.logger.trace(`Webhook cost: ${followWebhookResponse.data[0].cost} / ${followWebhookResponse.max_total_cost}`);
+    clientInfo.webhooks.push(followWebhookResponse.data[0].id);
     // TODO: setup webhooks
   }
 
   // Clean up a retired client
   // Removes webhooks and other associated
-  public cleanupClient(sessionId: string): void {
+  public async cleanupClient(sessionId: string): Promise<void> {
     const clientInfo = this.getClient(sessionId);
     if (clientInfo != null) {
       // TODO: remove active webhooks
       this.logger.warn(`Cleaning up client: ${clientInfo.sessionId}`);
+
+      // Clean up active webhooks
+      const client = new TwitchAPIClient();
+      for (const webhookId of clientInfo.webhooks) {
+        try {
+          await client.deleteEventSubSubscription(webhookId);
+          this.logger.debug(
+              `Deleted webhook for session: ${sessionId}, ${webhookId}`);
+        } catch (e) {
+          this.logger.error(`Failed to delete webhook: ${e}`);
+          this.logger.error((e as Error).stack);
+        }
+      }
+
+      // Remove from the client map
       this.clients.delete(sessionId);
     }
   }
